8c0fdfd2fcedaff9f57469150a2eaa4e7686b082
freedomevolves <samskinnerphd@gmail.com>
2015-12-07 11:03:53 -0500

    modify Tack controller actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,30 +1,19 @@
 class TasksController < ApplicationController
+  before_action :all_tasks, only: [:index, :create]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  # GET /tasks
-  # GET /tasks.json
-  def index
-    @tasks = Task.all
-  end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
-  def show
-  end
 
-  # GET /tasks/new
   def new
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
   def edit
   end
 
-  # POST /tasks
-  # POST /tasks.json
   def create
-    @task = Task.new(task_params)
+    #@task = Task.new(task_params)
+    @task = Task.create(task_params)
 
     respond_to do |format|
       if @task.save
@@ -37,8 +26,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
     respond_to do |format|
       if @task.update(task_params)
@@ -51,8 +38,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
     @task.destroy
     respond_to do |format|
@@ -62,7 +47,11 @@ class TasksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
+    def all_tasks
+      @tasks = Task.all
+    end
+
     def set_task
       @task = Task.find(params[:id])
     end
